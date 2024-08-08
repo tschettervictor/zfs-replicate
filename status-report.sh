f44a308352d8b76e72a3982282012773c4f70448
tschettervictor <85497460+tschettervictor@users.noreply.github.com>
2024-08-08 13:23:43 -0600

    Update status-report.sh
    Set default log path to be inside the script directory to keep everything in one place

--- status-report.sh
+++ status-report.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-# Check status of replication by reading last line of latest log file
 
-LOGS="/var/log/zfs-replicate"
+# Set default script directory
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "${SCRIPT}")
 
+# Check for existing logs
+if ! [ -e  ${SCRIPTPATH}/logs ]; then
+	echo "Log directory does not exist, can't check status."
+	exit 0
+fi
+
+# Set log directory
+LOGS="${SCRIPTPATH}/logs"
+
+# Retrieve latest log status
 RECENT_LOG_FILE=$(ls ${LOGS} | grep autorep- | tail -n 1)
 STATUS=$(tail -n 1 ${LOGS}/${RECENT_LOG_FILE})
 
